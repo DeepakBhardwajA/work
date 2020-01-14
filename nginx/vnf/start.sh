aaad8e3c2f43eb6ea4fad5e2904d17e2a93ca9de
Deepak Bhardwaj <m2deepak93@gmail.com>
2020-01-14 18:09:15 +0100

    files modified

--- nginx/vnf/start.sh
+++ nginx/vnf/start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 date > /tngbench_share/start.txt
 
-#echo "Starting Promethues exporter..."
-#python3 prometheus_exporter.py &#> exporter.log &
+# modify config using ENV vars
+sed -i 's/TCP_LISTEN_PORT/'"$TCP_LISTEN_PORT"'/g' /etc/nginx/nginx.conf
+sed -i 's/TCP_SERVERS/'"$TCP_SERVERS"'/g' /etc/nginx/nginx.conf
+sed -i 's/UDP_LISTEN_PORT/'"$UDP_LISTEN_PORT"'/g' /etc/nginx/nginx.conf
+sed -i 's/UDP_SERVERS/'"$UDP_SERVERS"'/g' /etc/nginx/nginx.conf
 
-nginx -f /etc/nginx/nginx.conf -D # &
+echo "Starting Promethues exporter..."
+python3 prometheus_exporter.py &#> exporter.log &
+
+nginx &
 
 echo "Nginx VNF started ..."
